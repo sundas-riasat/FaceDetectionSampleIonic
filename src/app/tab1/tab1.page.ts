eb22f76cedf538cd52ae68cf5a2d8ce036dfec03
Sundas Riasat <sundasriasat@gmail.com>
2020-06-11 21:50:46 +0500

    Completed, Time measuerment enabled

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -21,6 +21,7 @@ export class Tab1Page implements OnInit {
     public allowTakePhoto = true;
     public isMenuOpen = false;
     public hider;
+    public total = 0;
     url = '';
 
     @ViewChild('overlay') canvasOverlay: ElementRef;
@@ -45,7 +46,6 @@ export class Tab1Page implements OnInit {
 
     init(canvasOverlayNe) {
         this.platform.ready().then(() => {
-            console.log('platform ready', Date.now().toString());
             if (this.platform.is('cordova')) {
                 this.handleCameraPermission(() => {
                     this.initDetection(canvasOverlayNe);
@@ -75,50 +75,48 @@ export class Tab1Page implements OnInit {
                     const video = document.querySelector('video');
                     video.srcObject = stream;
                     this.setShowVideo();
-                    const htracker = new headtrackr.Tracker({detectionInterval: 50});
+                    const htracker = new headtrackr.Tracker({
+                        detectionInterval: 50,
+                        whitebalancing: false,
+                        headPosition: false,
+                        smoothing: false,
+                        retryDetection: true
+                    });
                     const canvas = document.querySelector('canvas');
                     htracker.init(video, canvas);
                     htracker.start();
-                    let intr = true;
-                    setInterval(() => {
-                        if (intr) {
-                            document.getElementById('detector').style.border = '5px solid red';
-                        }
-                    }, 500);
+                    // let intr = true;
+                    // setInterval(() => {
+                    //     if (intr) {
+                    //         document.getElementById('detector').style.border = '5px solid red';
+                    //     }
+                    // }, 500);
                     setTimeout(() => {
                         this.hider.style.display = 'none';
                     }, 500);
-                    document.addEventListener('facetrackingEvent', (event: any) => {
-                        if (!this.isMenuOpen) {
-                            // clear canvas
-                            overlayContext.clearRect(0, 0, this.width, this.height);
-                            //  once we have stable tracking, draw rectangle
-
-                            if (event.detection === 'CS') {
-                                // overlayContext.translate(event.x, event.y);
-                                // overlayContext.rotate(event.angle - (3.14 / 2));
-                                // overlayContext.strokeStyle = '#00CC00';
-                                // console.log((-(event.width / 2)) >> 0, (-(event.height / 2)) >> 0, event.width, event.height);
-                                // overlayContext.strokeRect((-(event.width / 2)) >> 0, (-(event.height / 2)) >> 0, event.width, event.height);
-                                // overlayContext.rotate((3.14 / 2) - event.angle);
-                                // overlayContext.translate(-event.x, -event.y);
-                                intr = false;
-                                setTimeout(() => {
-                                    intr = true;
-                                }, 500);
-                                document.getElementById('detector').style.border = '5px solid green';
-                                if (this.allowTakePhoto) {
-                                    this.takePhoto();
-                                    this.allowTakePhoto = false;
-                                    setTimeout(() => {
-                                        this.allowTakePhoto = true;
-                                        intr = true;
-                                    }, 2000);
-
-                                }
-                            }
-                        }
-                    });
+                    // document.addEventListener('facetrackingEvent', (event: any) => {
+                    //     if (!this.isMenuOpen) {
+                    //         overlayContext.clearRect(0, 0, this.width, this.height);
+                    //         if (event.detection === 'CS') {
+                    //             if (event.x < 400 && event.y < 550){
+                    //                 intr = false;
+                    //                 setTimeout(() => {
+                    //                     intr = true;
+                    //                 }, 500);
+                    //                 document.getElementById('detector').style.border = '5px solid green';
+                    //                 if (this.allowTakePhoto) {
+                    //                     this.takePhoto();
+                    //                     this.allowTakePhoto = false;
+                    //                     setTimeout(() => {
+                    //                         this.allowTakePhoto = true;
+                    //                         intr = true;
+                    //                     }, 2000);
+                    //
+                    //                 }
+                    //             // }
+                    //         }
+                    //     }
+                    // });
                 }, (err) => {
                     console.log('err: ', err);
                 }
@@ -147,16 +145,6 @@ export class Tab1Page implements OnInit {
     }
 
     public handleCameraPermission(cb) {
-        // const permissions = cordova.plugins.permissions;
-        // permissions.requestPermissions([
-        //     permissions.CAMERA,
-        //     permissions.MODIFY_AUDIO_SETTINGS,
-        //     permissions.RECORD_AUDIO
-        // ], (status) => {
-        //     cb(status);
-        // }, (err) => {
-        //     console.log('error on request permissions: ', err);
-        // });
         this.androidPermissions.requestPermissions([
             this.androidPermissions.PERMISSION.CAMERA,
             this.androidPermissions.PERMISSION.MODIFY_AUDIO_SETTINGS,
@@ -169,31 +157,41 @@ export class Tab1Page implements OnInit {
     }
 
     takePhoto() {
-
-        const canvas = this.canvasImage.nativeElement;
-        const videoGet = document.querySelector('video');
-        canvas.width = 270;
-        canvas.height = 360;
-        canvas.getContext('2d').drawImage(videoGet, 0, 0, 270, 360);
-        const img = canvas.toDataURL('image/jpeg', 1.0);
-        this.imageSrc = img;
-        this.base64ToArrayBuffer(img, canvas.width, canvas.height);
-
-    }
-
-    base64ToArrayBuffer(base64, w, h) {
-        const startTime = (new Date()).getTime();
-        let endTime;
-        this.apiService.postImage(base64, w, h)?.subscribe((x: any) => {
-            // this.presentToast(JSON.stringify(x.message) );
-        }, err => {
-            if (!err.error.text.toString().includes('Error')) {
-                this.presentToast(err.error.text);
-                // endTime = (new Date()).getTime();
-                // this.presentToast('Took ' + (endTime - startTime) + 'ms');
+        this.total = 0;
+        var min = 0;
+        var max = 0;
+        this.presentToast('Recognizing...');
+        for (let i = 0; i < 50; i++) {
+            var start = Date.now();
+            const canvas = this.canvasImage.nativeElement;
+            const videoGet = document.querySelector('video');
+            canvas.width = 270;
+            canvas.height = 360;
+            canvas.getContext('2d').drawImage(videoGet, 0, 0, 270, 360);
+            const img = canvas.toDataURL('image/jpeg', 1.0);
+            this.imageSrc = img;
+            this.apiService.postImage(img, canvas.width, canvas.height)?.subscribe((x: any) => {
+            }, err => {
+                // if (!err.error.text.toString().includes('Error')) {
+                //     this.presentToast(err.error.text);
+                // } else {
+                //     this.presentToast('Not recognized');
+                // }
+            });
+            var end = Date.now();
+            if (i === 0) {
+                min = max = (end - start);
+            } else {
+                if ((end - start) < min) {
+                    min = (end - start);
+                }
+                if ((end - start) > max) {
+                    max = (end - start);
+                }
             }
-        });
-
+            this.total += (end - start);
+        }
+        this.presentToast('Average: ' + (this.total / 50).toString() + ' Min: ' + min + ' Max: '+ max);
     }
 
     setURL() {
